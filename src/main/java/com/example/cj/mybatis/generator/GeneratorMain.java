ed09a7eabcc046c051a9afa7fd5ca53e393c5569
david <425800970@qq.com>
2020-03-31 16:42:44 +0800

    mybatis generator使用demo

---
+++ src/main/java/com/example/cj/mybatis/generator/GeneratorMain.java
@@ -0,0 +1,30 @@
+package com.example.cj.mybatis.generator;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class GeneratorMain {
+    public static void generator() throws Exception{
+
+        List<String> warnings = new ArrayList<String>();
+        boolean overwrite = true;
+        //指定逆向工程配置文件
+        InputStream resourceAsStream = GeneratorMain.class.getClassLoader().getResourceAsStream("generatorConfig.xml");
+        ConfigurationParser cp = new ConfigurationParser(warnings);
+        Configuration config = cp.parseConfiguration(resourceAsStream);
+        DefaultShellCallback callback = new DefaultShellCallback(overwrite);
+        MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config,
+                callback, warnings);
+        myBatisGenerator.generate(null);
+    }
+
+    public static void main(String[] args) throws Exception {
+        GeneratorMain.generator();
+    }
+}
